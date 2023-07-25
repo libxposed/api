74534aa0c1511f963dda33ff839504440096822e
LoveSy <shana@zju.edu.cn>
2023-07-26 01:30:57 +0800

    XposedHooker should also target TYPE_USE

--- api/src/main/java/io/github/libxposed/api/annotations/XposedHooker.java
+++ api/src/main/java/io/github/libxposed/api/annotations/XposedHooker.java
@@ -6,6 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
+@Target({ElementType.TYPE, ElementType.TYPE_USE})
 public @interface XposedHooker {
 }
