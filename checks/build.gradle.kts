6edcdec6cb9ea11857fd90fef36ba34efb12ae3b
Siavash <siavash@gmail.com>
2024-10-30 15:59:36 +0330

    Update build.gradle.kts
    Update JAVA to 22 to match kotlin

--- checks/build.gradle.kts
+++ checks/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_22
+    targetCompatibility = JavaVersion.VERSION_22
 }
 
 dependencies {
